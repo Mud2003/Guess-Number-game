880fdc0e9629bd159251cf1a793121d8352d0dad
Muditha Madusith <bmmadusith2003@gmail.com>
2022-07-28 12:13:13 +0530

    Add the files to repo

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+def guess_number():
+    hidden_number = random.randint(1, 10)
+    your_input = 0
+
+    while your_input != hidden_number:
+        your_input = int(input("Enter 1 - 10 number:- "))
+
+        if your_input < hidden_number:
+            print("Too low. Try again!")
+        if your_input > hidden_number:
+            print("Too high. Try again!")
+
+    print("You won!")
+
+guess_number()
